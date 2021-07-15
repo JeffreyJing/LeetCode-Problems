f08cddfa332bbab4ea3cb99653ad6aba46ba21f0
JeffreyJing <jjingdn@gmail.com>
2021-07-15 12:13:27 -0700

    Time: 2 ms (98.09%), Space: 48 MB (49.90%) - LeetHub

--- count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
+++ count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
@@ -15,21 +15,54 @@
  */
 class Solution {
     
-    int count = 0;
     public int goodNodes(TreeNode root) {
-        goodNodes(root, root.val);
-        return count;
+        return goodNodes(root, root.val);
     }
     
-    private void goodNodes(TreeNode current, int biggest) {
+    private int goodNodes(TreeNode current, int currentGreatest) {
+        if (current != null) {
+            int currentCount = 0;
+            if (current.val >= currentGreatest) {
+                currentCount++;
+                currentGreatest = current.val;
+            }
+            
+            return currentCount + goodNodes(current.left, currentGreatest) + goodNodes(current.right, currentGreatest);
+        }
+        
+        return 0;
+    }
+    
+    /*
+    public int goodNodes(TreeNode root) {
+        return goodNodes(root, root.val);
+    }
+    
+    private int goodNodes(TreeNode current, int biggest) {
+        if (current != null) {
+            biggest = biggest < current.val ? current.val : biggest;
+            return (current.val >= biggest ? 1 : 0) + goodNodes(current.left, biggest) + goodNodes(current.right, biggest);
+        }
+        
+        return 0;
+    }
+    */
+    
+    /* public int goodNodes(TreeNode root) {
+        int[] count = new int[1];
+        goodNodes(root, root.val, count);
+        return count[0];
+    }
+    
+    private void goodNodes(TreeNode current, int biggest, int[] count) {
         if (current != null) {
             if (current.val >= biggest) {
-                count++;
+                count[0]++;
                 biggest = current.val;
             }
             
-            goodNodes(current.left, biggest);
-            goodNodes(current.right, biggest);
+            goodNodes(current.left, biggest, count);
+            goodNodes(current.right, biggest, count);
         }
-    }
+    } */
 }
