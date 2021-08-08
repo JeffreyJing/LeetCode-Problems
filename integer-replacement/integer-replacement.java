98d473c0a51bceef1ea5b37f5ea536aff2d0f8b3
JeffreyJing <jjingdn@gmail.com>
2021-08-08 14:23:35 -0700

    Time: 4 ms (34.80%), Space: 35.6 MB (77.17%) - LeetHub

---
+++ integer-replacement/integer-replacement.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int integerReplacement(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        
+        if (n % 2 == 0) {
+            return integerReplacement(n / 2) + 1;
+        }
+        
+        return Math.min(integerReplacement(n / 2), integerReplacement(n / 2 + 1)) + 2;
+    }
+}
