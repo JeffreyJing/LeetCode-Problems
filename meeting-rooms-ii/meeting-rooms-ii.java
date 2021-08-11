b9d7c35b3d940055d39292fc5ba5b7ee21a46558
JeffreyJing <jjingdn@gmail.com>
2021-08-10 22:29:30 -0700

    Time: 6 ms (76.30%), Space: 38.5 MB (89.73%) - LeetHub

--- meeting-rooms-ii/meeting-rooms-ii.java
+++ meeting-rooms-ii/meeting-rooms-ii.java
@@ -1,38 +1,17 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0])); //sort by start times
-        //if the input is [0,30],[15,20],[5,10] --> after soring, it becomes [0,30],[5,10],[15,20]
-        
-        PriorityQueue<Integer> queue = new PriorityQueue<>(); //create a priority queue
-        //add end time of first sorted interval to the priority queue i.e [30] in this example
-        queue.offer(intervals[0][1]); 
-		
-        for(int i=1; i< intervals.length; i++){ //start for loop from 1st index
-		
-		 //in each iteration, compare start time with peek of the priority queue. For example 5 >= 30? (in first interation)
-            if(intervals[i][0] >= queue.peek()) {  
-				queue.poll();  
-			}
-            queue.offer(intervals[i][1]); //add the current interval's end time to the queue [10] (in first iteration)
-        }
-        return queue.size(); //return the queue size
-    }
-}
-
-/*
-    public int minMeetingRooms(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         PriorityQueue<Integer> startingTimes = new PriorityQueue<Integer>();
-        startingTimes.add(intervals[0][1]);
+        startingTimes.offer(intervals[0][1]);
         for (int i = 1; i < intervals.length; i++) {
-            if (intervals[i][0] > startingTimes.peek()) {
+            if (intervals[i][0] >= startingTimes.peek()) {
                 startingTimes.poll();
             }
             
-            startingTimes.add(intervals[i][1]);
+            startingTimes.offer(intervals[i][1]);
             
         }
         
         return startingTimes.size();
     }
-*/
+}
