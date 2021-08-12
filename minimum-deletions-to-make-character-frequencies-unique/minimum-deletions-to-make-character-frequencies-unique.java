a7d8a0e02b64af305e3da7ce129edb104bf6e745
JeffreyJing <jjingdn@gmail.com>
2021-08-11 21:16:16 -0700

    Time: 15 ms (63.90%), Space: 40.2 MB (16.73%) - LeetHub

--- minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
+++ minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,29 +1,26 @@
 class Solution {
     public int minDeletions(String s) {
-        Map<Character, Integer> charToCount = new HashMap<Character, Integer>();
+        int[] letterInventory = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            char currentChar = s.charAt(i);
-            if (!charToCount.containsKey(currentChar)) {
-                charToCount.put(currentChar, 1);
-            } else {
-                charToCount.put(currentChar, charToCount.get(currentChar) + 1);
-            }
+            letterInventory[s.charAt(i) - 97]++;
         }
         
         int count = 0;
         Set<Integer> frequencies = new HashSet<Integer>();
-        for (Character letter : charToCount.keySet()) {
-            int frequency = charToCount.get(letter);
-            if (!frequencies.contains(frequency)) {
-                frequencies.add(frequency);
-            } else {
-                while (frequencies.contains(frequency)) {
-                    frequency--;
-                    count++;
-                } 
-                
-                if (frequency != 0) {
+        for (int i = 0; i < 26; i++) {
+            int frequency = letterInventory[i];
+            if (frequency != 0) {
+                if (!frequencies.contains(frequency)) {
                     frequencies.add(frequency);
+                } else {
+                    while (frequencies.contains(frequency)) {
+                        frequency--;
+                        count++;
+                    } 
+
+                    if (frequency != 0) {
+                        frequencies.add(frequency);
+                    }
                 }
             }
         }
