6d7e9b9b2b54516055ac051aafd93647c79557d3
JeffreyJing <jjingdn@gmail.com>
2021-08-11 20:59:16 -0700

    Time: 0 ms (100.00%), Space: 37.3 MB (60.33%) - LeetHub

--- backspace-string-compare/backspace-string-compare.java
+++ backspace-string-compare/backspace-string-compare.java
@@ -1,5 +1,49 @@
 class Solution {
     public boolean backspaceCompare(String first, String second) {
+        int firstIndex = first.length() - 1;
+        int secondIndex = second.length() - 1;
+        int firstSkips = 0;
+        int secondSkips = 0;
+        while (firstIndex >= 0 || secondIndex >= 0) {
+            while (firstIndex >= 0) {
+                if (first.charAt(firstIndex) == '#') {
+                    firstSkips++;
+                    firstIndex--;
+                } else if (firstSkips > 0) {
+                    firstSkips--;
+                    firstIndex--;
+                } else {
+                    break;
+                }
+            }
+            
+            while (secondIndex >= 0) {
+                if (second.charAt(secondIndex) == '#') {
+                    secondSkips++;
+                    secondIndex--;
+                } else if (secondSkips > 0) {
+                    secondSkips--;
+                    secondIndex--;
+                } else {
+                    break;
+                }
+            }
+            
+            if (firstIndex >= 0 && secondIndex >= 0 && first.charAt(firstIndex) != second.charAt(secondIndex)) {
+                return false;
+            }
+            
+            if ((firstIndex >= 0) != (secondIndex >= 0)) {
+                return false;
+            }
+            
+            firstIndex--;
+            secondIndex--;
+        }
+        return true;
+    }
+    
+    /* public boolean backspaceCompare(String first, String second) {
         return backspace(first).equals(backspace(second));
     }
     
@@ -18,4 +62,5 @@ class Solution {
         
         return letters;
     }
+    */
 }
