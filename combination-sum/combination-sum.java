6ba3a993f182017bcdfd6009ee35574dbf26a0e7
JeffreyJing <jjingdn@gmail.com>
2021-08-29 22:45:19 -0700

    Time: 2 ms (98.47%), Space: 39.1 MB (83.15%) - LeetHub

--- combination-sum/combination-sum.java
+++ combination-sum/combination-sum.java
@@ -1,26 +1,30 @@
 class Solution {
+    
+    // use a LinkedList
+    // sort the given array
+    // use backtracking to fill the list of lists
+    
+    // backtracking: if the target is reached (== 0), add the current list
+    // to the result.
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         Arrays.sort(candidates);
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        backtrack(target, new LinkedList<Integer>(), 0, candidates, result);
+        backtrack(candidates, result, new LinkedList<Integer>(), target, 0);
         return result;
     }
     
-    private void backtrack(int remain, LinkedList<Integer> comb, int start, int[] candidates, List<List<Integer>> result) {
-        if (remain >= 0) {
-            if (remain == 0) {
-                result.add(new ArrayList<Integer>(comb));
-            } else {
-                for (int i = start; i < candidates.length; i++) {
-                    int current = candidates[i];
-                    if (remain >= current) {
-                        comb.add(current);
-                        backtrack(remain - current, comb, i, candidates, result);
-                        comb.removeLast();
-                    }
+    private void backtrack(int[] candidates, List<List<Integer>> result, LinkedList<Integer> currentList, int remaining, int start) {
+        if (remaining == 0) {
+            result.add(new ArrayList<Integer>(currentList));
+        } else {
+            for (int i = start; i < candidates.length; i++) {
+                int current = candidates[i];
+                if (remaining >= current) {
+                    currentList.add(current);
+                    backtrack(candidates, result, currentList, remaining - current, i);
+                    currentList.removeLast();
                 }
             }
-        }  
+        }
     }
-    
 }
