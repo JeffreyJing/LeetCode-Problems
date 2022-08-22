c285f10311b3e0d056a335463828ddb29f05fd5e
JeffreyJing <jjingdn@gmail.com>
2022-08-21 18:06:17 -0700

    Time: 0 ms (100.00%), Space: 41.9 MB (93.86%) - LeetHub

---
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.java
@@ -0,0 +1,70 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        ListNode current = head.next;
+        ListNode prev = head;
+        ListNode uniques = null;
+        ListNode uniquesCurrent = uniques;
+        while (prev != null && current != null) {
+            if (prev.val == current.val) {
+                prev = findNextVal(current);
+                if (prev == null) {
+                    return uniques;
+                }
+                
+                current = prev.next;
+            } else {
+                if (uniques == null) {
+                    uniques = prev;
+                    uniques.next = null;
+                    uniquesCurrent = uniques;
+                } else {
+                    uniquesCurrent.next = prev;
+                    uniquesCurrent = uniquesCurrent.next;
+                    uniquesCurrent.next = null;
+                }
+                
+                prev = current;
+                current = current.next;
+            }
+        }
+        
+        if (uniquesCurrent == null) {
+            return prev;
+        }
+        
+        if (prev.val != uniquesCurrent.val) {
+            uniquesCurrent.next = prev;
+            uniquesCurrent.next.next = null;
+        } else {
+            uniquesCurrent.next = null;
+        }
+        
+        return uniques;
+    }
+    
+    private ListNode findNextVal(ListNode current) {
+        while (current != null && current.next != null) {
+            if (current.val != current.next.val) {
+                return current.next;
+            } else {
+                current = current.next;
+            }
+        }
+        
+        return null;
+    }
+}
